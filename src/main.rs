ce6c3b743e4dac638c7c42878bde6913f0157e4f
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-05-01 09:51:21 +0200

    🔄 Refactorizado main.rs y mail.rs para mejorar formato de código y consistencia

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
 mod models;
 
+use models::{Bot, Configuration, MatrixClient};
+use serde_json::Value;
 use std::sync::Arc;
 use std::{process, time};
-use tracing_subscriber::{
-    fmt,
-    EnvFilter,
-    layer::SubscriberExt,
-    util::SubscriberInitExt,
-};
-use models::{
-    Configuration,
-    MatrixClient,
-    Bot,
-};
-use serde_json::Value;
-use tracing::{info, debug, error};
+use tracing::{debug, error, info};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
-async fn main(){
+async fn main() {
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
         .init();
     info!("Start");
-    tokio::spawn( async {
-        let configuration = match Configuration::read().await{
+    tokio::spawn(async {
+        let configuration = match Configuration::read().await {
             Ok(configuration) => Arc::new(configuration),
-            Err(e) =>{
+            Err(e) => {
                 println!("Error. Can not read configuration: {}", e);
                 process::exit(0);
             }
@@ -36,59 +27,68 @@ async fn main(){
         let duration = time::Duration::from_secs(sleep_time);
         let imap_server = configuration.get_imap_server();
         let matrix_client = configuration.get_matrix_client();
-        loop{
+        loop {
             debug!("==== START LOOP ====");
-            match imap_server.get_unread_mails().await{
+            match imap_server.get_unread_mails().await {
                 Ok(mails) => {
-                    for mail in mails.as_slice(){
+                    for mail in mails.as_slice() {
                         debug!("{}", &mail);
-                        match matrix_client.post_to_email_room(&mail.to_string()).await{
+                        match matrix_client.post_to_email_room(&mail.to_string()).await {
                             Ok(response) => debug!(response),
-                            Err(error_message) => error!(error_message)
-
+                            Err(error_message) => error!(error_message),
                         }
                     }
-                    if mails.is_empty(){
+                    if mails.is_empty() {
                         debug!("Not found any mail");
                     }
-                },
+                }
                 Err(message) => error!("{}", message),
             };
             debug!("==== END LOOP ====");
             tokio::time::sleep(duration).await;
         }
     });
-    let mut configuration2 = match Configuration::read().await{
+    let mut configuration2 = match Configuration::read().await {
         Ok(configuration) => configuration,
-        Err(e) =>{
+        Err(e) => {
             println!("Error. Can not read configuration: {}", e);
             process::exit(0);
         }
     };
     loop {
         debug!("===== Leo =====");
-        match configuration2.matrix_client.sync().await{
+        match configuration2.matrix_client.sync().await {
             Ok(response) => {
                 debug!("Response: {:?}", response);
                 if let Some(response) = response {
-                    match configuration2.save().await{
+                    match configuration2.save().await {
                         Ok(()) => {
                             debug!("Configuration saved");
                             debug!("Configuration: {:?}", &configuration2);
-                        },
+                        }
                         Err(e) => error!("Cant save configuration: {}", e),
                     };
-                    if let Some((room, command)) = process_response(&response, &configuration2.matrix_client){
+                    if let Some((room, command)) =
+                        process_response(&response, &configuration2.matrix_client)
+                    {
                         debug!(command);
                         if let Some(message) = Bot::response(&command).await {
-                            if configuration2.matrix_client.chat_room == room{
-                                match &configuration2.matrix_client.post_to_chat_room(&message).await{
+                            if configuration2.matrix_client.chat_room == room {
+                                match &configuration2
+                                    .matrix_client
+                                    .post_to_chat_room(&message)
+                                    .await
+                                {
                                     Ok(response) => debug!("Response: {}", response),
                                     Err(e) => error!("Error: {}", e),
                                 }
                             }
-                            if configuration2.matrix_client.email_room == room{
-                                match &configuration2.matrix_client.post_to_email_room(&message).await{
+                            if configuration2.matrix_client.email_room == room {
+                                match &configuration2
+                                    .matrix_client
+                                    .post_to_email_room(&message)
+                                    .await
+                                {
                                     Ok(response) => debug!("Response: {}", response),
                                     Err(e) => error!("Error: {}", e),
                                 }
@@ -96,11 +96,11 @@ async fn main(){
                         }
                     }
                 }
-            },
+            }
             Err(error) => {
                 error!("Can not sync: {:#}", error);
                 let mut next_err = error.source();
-                while next_err.is_some(){
+                while next_err.is_some() {
                     error!("caused by: {:#}", next_err.unwrap());
                     next_err = next_err.unwrap().source();
                 }
@@ -112,43 +112,48 @@ async fn main(){
     }
 }
 
-fn process_response(value: &Value, matrix_client: &MatrixClient) -> Option<(String, String)>{
+fn process_response(value: &Value, matrix_client: &MatrixClient) -> Option<(String, String)> {
     let sender = matrix_client.get_sender_id();
 
-    if let Some(rooms) = value.get("rooms")
+    if let Some(rooms) = value
+        .get("rooms")
         .and_then(|rooms| rooms.get("join"))
-        .and_then(|selected_room| selected_room.as_object()) 
+        .and_then(|selected_room| selected_room.as_object())
     {
         for (room, room_value) in rooms {
             let compare_room = room.split(':').next().unwrap();
             debug!("Compare room: {compare_room}");
-            debug!("=== {} <=> {} / {} / {} ===", matrix_client.chat_room, compare_room, room, room_value);
-            if compare_room != matrix_client.email_room && compare_room != matrix_client.chat_room{
-                return None
+            debug!(
+                "=== {} <=> {} / {} / {} ===",
+                matrix_client.chat_room, compare_room, room, room_value
+            );
+            if compare_room != matrix_client.email_room && compare_room != matrix_client.chat_room {
+                return None;
             }
-            if let Some(timeline) = room_value.get("timeline")
+            if let Some(timeline) = room_value
+                .get("timeline")
                 .and_then(|timeline| timeline.get("events"))
-                .and_then(|events| events.as_array()) 
+                .and_then(|events| events.as_array())
             {
                 for event in timeline {
-                    if let Some(current_sender) = event.get("sender")
-                            .and_then(|sender| sender.as_str()) {
+                    if let Some(current_sender) =
+                        event.get("sender").and_then(|sender| sender.as_str())
+                    {
                         debug!("Sender: {}", current_sender);
                         if sender == current_sender {
                             return None;
                         }
                     }
-                    if let Some(body) = event.get("content")
-                        .and_then(|content| {
-                            debug!("Evento: {:?}", content);
-                            content.get("msgtype")
-                                .and_then(|msgtype| msgtype.as_str())
-                                .filter(|&msgtype| msgtype == "m.text")
-                                .and_then(|_| content.get("body"))
-                                .and_then(|body| body.as_str())
-                        }) 
-                    {
-                        return Some((compare_room.to_string(),body.to_string()));
+                    if let Some(body) = event.get("content").and_then(|content| {
+                        debug!("Evento: {:?}", content);
+                        content
+                            .get("msgtype")
+                            .and_then(|msgtype| msgtype.as_str())
+                            .filter(|&msgtype| msgtype == "m.text")
+                            .and_then(|_| content.get("body"))
+                            .and_then(|body| body.as_str())
+                    }) {
+                        return Some((compare_room.to_string(), body.to_string()));
                     }
                 }
             }
--- src/models/mail.rs
+++ src/models/mail.rs
@@ -1,32 +1,29 @@
-use std::fmt::Display;
+use mail_parser::{Addr, Address, Group, Message};
 use std::fmt;
-use mail_parser::{Message, Addr, Address, Group};
+use std::fmt::Display;
 
-pub struct HeaderMail{
+pub struct HeaderMail {
     id: u32,
     reg: String,
     from: String,
     subject: String,
 }
-pub struct Mail{
+pub struct Mail {
     header: HeaderMail,
     body: String,
 }
 
 impl Mail {
-    pub fn new(id: u32, content: &Message) -> Self{
+    pub fn new(id: u32, content: &Message) -> Self {
         let header = HeaderMail::new(id, content);
         let body = Self::get_body(content);
-        Self{
-            header,
-            body,
-        }
+        Self { header, body }
     }
 
-    fn get_body(content: &Message) -> String{
+    fn get_body(content: &Message) -> String {
         let mut s = String::new();
-        for part in content.text_bodies(){
-            if part.is_text() && part.text_contents().is_some(){
+        for part in content.text_bodies() {
+            if part.is_text() && part.text_contents().is_some() {
                 s.push_str(part.text_contents().unwrap())
             }
         }
@@ -35,46 +32,46 @@ impl Mail {
 }
 
 impl HeaderMail {
-    pub fn new(id: u32, content: &Message) -> Self{
+    pub fn new(id: u32, content: &Message) -> Self {
         let reg = content.message_id().unwrap().to_string();
         let from = Self::get_address(content.from());
         let subject = content.subject().unwrap_or("").to_string();
-        Self{
+        Self {
             id,
             reg,
             from,
             subject,
         }
     }
-    fn get_from_groups(groups: &Vec<Group>) -> String{
+    fn get_from_groups(groups: &Vec<Group>) -> String {
         let mut result = Vec::new();
-        for group in groups{
+        for group in groups {
             result.push(Self::get_from_addresses(&group.addresses));
         }
         result.join(", ")
     }
 
-    fn get_from_addresses(addresses: &Vec<Addr>) -> String{
+    fn get_from_addresses(addresses: &Vec<Addr>) -> String {
         let mut result = Vec::new();
-        for address in addresses{
+        for address in addresses {
             result.push(Self::get_from_address(address))
         }
         result.join(", ")
     }
-    fn get_from_address(address: &Addr) -> String{
-        let name = match &address.name{
+    fn get_from_address(address: &Addr) -> String {
+        let name = match &address.name {
             Some(name) => name.to_string(),
             None => "".to_string(),
         };
-        let mail = match &address.address{
+        let mail = match &address.address {
             Some(mail) => mail.to_string(),
             None => "".to_string(),
         };
         format!("{} <{}>", name, mail)
     }
 
-    fn get_address(address: Option<&Address>) -> String{
-        match address{
+    fn get_address(address: Option<&Address>) -> String {
+        match address {
             Some(address) => match address {
                 Address::List(addresses) => Self::get_from_addresses(addresses),
                 Address::Group(groups) => Self::get_from_groups(groups),
@@ -82,19 +79,24 @@ impl HeaderMail {
             None => "".to_string(),
         }
     }
-
 }
 
 impl Display for HeaderMail {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Id: {}\nReg: {}\nFrom: {}\nSubject: {}",
-            self.id, self.reg, self.from, self.subject)
+        write!(
+            f,
+            "Id: {}\nReg: {}\nFrom: {}\nSubject: {}",
+            self.id, self.reg, self.from, self.subject
+        )
     }
 }
 
 impl Display for Mail {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Id: {}\nReg: {}\nFrom: {}\nSubject: {}\nBody: {}",
-            self.header.id, self.header.reg, self.header.from, self.header.subject, self.body)
+        write!(
+            f,
+            "Id: {}\nReg: {}\nFrom: {}\nSubject: {}\nBody: {}",
+            self.header.id, self.header.reg, self.header.from, self.header.subject, self.body
+        )
     }
 }
